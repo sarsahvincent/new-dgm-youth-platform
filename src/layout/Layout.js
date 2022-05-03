0f7c3befa093b0b3bfc9f7cbf93432ef75a31e2e
Vincentsarsah <avioncent@yahoo.com>
2022-05-03 09:32:13 +0000

    update

--- src/layout/Layout.js
+++ src/layout/Layout.js
@@ -171,7 +171,6 @@ export default function MiniDrawer({ layout }) {
               <MenuIcon />
             </IconButton>
           ) : null}
-
           <div>
             <Link to="/">
               <Typography
